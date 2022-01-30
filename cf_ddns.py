7691a3c74740e0ef1945e46ea94bf45642a170d8
nekoreco <healneno@gmail.com>
2022-01-30 17:55:17 +0800

    Update cf_ddns.py

--- cf_ddns.py
+++ cf_ddns.py
@@ -8,10 +8,18 @@ import requests
 current_folder = os.path.abspath(os.path.dirname(__file__))
 
 
-def get_config():
+def get_configs():
     config_path = os.path.join(current_folder,'cf_config.json')
     with codecs.open(config_path,'rb',encoding='utf-8') as f:
-        return json.load(f)
+        configs =  json.load(f)
+
+    if isinstance(configs, dict):
+        configs = [configs]
+
+    if isinstance(configs, list):
+        raise Exception('cf_config.json file error')
+
+    return configs
 
 
 def get_ip(ip_get_url):
@@ -27,27 +35,28 @@ def set_ip(the_ip):
     sets the ip in via cloudflare api
     """
 
-    config = get_config()
+    configs = get_configs()
 
-    url = (
-        "https://api.cloudflare.com/client/v4/zones/%(zone_id)s/dns_records/%(record_id)s"
-        % {"zone_id": config.get("ZONE_ID"), "record_id": config.get("RECORD_ID")}
-    )
+    for config in configs:
+        url = (
+                "https://api.cloudflare.com/client/v4/zones/%(zone_id)s/dns_records/%(record_id)s"
+                % {"zone_id": config.get("ZONE_ID"), "record_id": config.get("RECORD_ID")}
+        )
 
-    headers = {
-        "Authorization":"Bearer "+config.get("API_KEY"),
-        "Content-Type": "application/json",
-    }
+        headers = {
+            "Authorization": "Bearer " + config.get("API_KEY"),
+            "Content-Type": "application/json",
+        }
 
-    payload = {
-        "type": "A",
-        "name": config.get("RECORD_NAME"),
-        "content": the_ip,
-        "proxied":False
-    }
-    response = requests.patch(url, headers=headers, data=json.dumps(payload))
-    print(response.content)
-    print(response.status_code)
+        payload = {
+            "type": "A",
+            "name": config.get("RECORD_NAME"),
+            "content": the_ip,
+            "proxied": False
+        }
+        response = requests.patch(url, headers=headers, data=json.dumps(payload))
+        print(response.content)
+        print(response.status_code)
 
 
 def main():
